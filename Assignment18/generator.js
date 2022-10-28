6c98a05d20229d5f282d1d58539486f5e48c475d
sumana10 <sumanaonlyme@gmail.com>
2022-10-28 17:26:20 +0530

    changes in generator

--- Assignment18/generator.js
+++ Assignment18/generator.js
@@ -1,40 +1,45 @@
+ 
+//ES6 introduced a new way of working with functions and iterators in the form of Generators(or generator functions).
+
+//A generator is a function that can stop midway and then continue from where it stopped.
 
 
-// ES6 introduced a new way of working with functions and iterators in the form of Generators (or generator functions). 
 
-
-// A generator is a function that can stop midway and then continue from where it stopped.
-
-function* generatorFunction() { // Line 1
+function* generatorFunction(){
   console.log('This will be executed first.');
-  yield 'Hello, ';   // Line 2
-  console.log('I will be printed after the pause');  
+  yield 'Hello,';
+  console.log('I will be printed after apuse');
   yield 'World!';
 }
-const generatorObject = generatorFunction(); // Line 3
-console.log(generatorObject.next().value); // Line 4
-console.log(generatorObject.next().value); // Line 5
-console.log(generatorObject.next().value); // Line 6
 
-// A generator function always returns a generator object. 
 
-// The generator object is an iterator. 
+const generatorObject = generatorFunction();
+console.log(generatorObject.next().value);
+console.log(generatorObject.next().value);
+console.log(generatorObject.next().value);
+
+ 
+//A generator function always returns a generator object.
+ 
+//The generator object is an iterator.
 
 // So you can use it in for-of loops or other functions accepting an iterable.
 
 function* multiplying(num1, num2) { // Line 1
   console.log('This will be executed first.');
-  yield `${num1*num2}`;   // Line 2
-  // console.log('I will be printed after the pause');  
-  // yield 'World!';
+  yield `${num1 * num2}`;   // Line 2
+  console.log('I will be printed after the pause');  
+  yield  `${num1 + num2}`; 
 }
 const multiplyingObject = multiplying(2,2); // Line 3
 console.log(multiplyingObject.next().value); // Line 4
+console.log(multiplyingObject.next().value); // Line 4
 
 //infinite series of number
-function* natural() {
+
+function* natural(){
   let i = 1;
-  while (true) {
+  while(true){
     yield i++;
   }
 }
@@ -54,9 +59,19 @@ function* generateException() {
   }
 }
 
+function* generatorException(){
+  while(true){
+    try{
+      yield 42;
+    }catch(e){
+      console.log('Error caught!')
+    }
+  }
+}
+
 const g = generateException();
 g.next();
 // { value: 42, done: false }
-g.throw(new Error('Something went wrong'));
+g.throw(new Error());
 // "Error caught!"
 // { value: 42, done: false }
